6f7f8028934a0c7e070d966784da29c36b154715
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-03 13:25:24 +0900

    Add enter & exit events to trays

--- source/scrollerful/script.js
+++ source/scrollerful/script.js
@@ -1,10 +1,16 @@
 (function(){'use strict';var style = ":root{--scrollerful-delay: 0s}.scrollerful{min-height:100%}.scrollerful--snap,.scrollerful__snap-page,.scrollerful__snap-page body{scroll-snap-type:y proximity}.scrollerful--snap,.scrollerful__snap-page{overflow-y:auto}.scrollerful--snap .scrollerful__tray,.scrollerful__snap-page .scrollerful__tray{scroll-snap-align:start end}.scrollerful--snap{height:100%}.scrollerful__plate{align-items:center;display:flex;flex-flow:column;height:100lvh;justify-content:center;max-height:100%;overflow:hidden;perspective-origin:center;perspective:150px;position:sticky;top:0;transform-style:preserve-3d}.scrollerful__sprite{animation-delay:calc(var(--scrollerful-progress-outer, 0)*-100s + var(--scrollerful-delay, 0));animation-duration:100s;animation-fill-mode:both;animation-play-state:paused;animation-timing-function:linear}.scrollerful__tray{height:300lvh}.scrollerful__tray--padding{height:100lvh}";const SCRIPT_NAME = 'scrollerful';
 
-const CSS_CLASS_SNAP = `.${SCRIPT_NAME}--snap`;
-const CSS_CLASS_TRAY = `.${SCRIPT_NAME}__tray`;
+const CSS_CLASS_INSIDE_INNER = `${SCRIPT_NAME}--inside--inner`;
+const CSS_CLASS_INSIDE_OUTER = `${SCRIPT_NAME}--inside--outer`;
 const CSS_PROP_PROGRESS_INNER = `--${SCRIPT_NAME}-progress-inner`;
 const CSS_PROP_PROGRESS_OUTER = `--${SCRIPT_NAME}-progress-outer`;
+const EVENT_INNER_ENTER = `${SCRIPT_NAME}innerenter`;
+const EVENT_INNER_EXIT = `${SCRIPT_NAME}innerexit`;
+const EVENT_OUTER_ENTER = `${SCRIPT_NAME}outerenter`;
+const EVENT_OUTER_EXIT = `${SCRIPT_NAME}outerexit`;
 const EVENT_SCROLL = `${SCRIPT_NAME}scroll`;
+const SEL_SNAP = `.${SCRIPT_NAME}--snap`;
+const SEL_TRAY = `.${SCRIPT_NAME}__tray`;
 const STYLE_EL_ID = `${SCRIPT_NAME}_style`;
 
 const getStyleEl = () => document.getElementById(STYLE_EL_ID);
@@ -77,24 +83,67 @@ const setStyleVars = ({
 	target.style.setProperty(CSS_PROP_PROGRESS_OUTER, outer);
 };
 
-const scroll = event => {
-	Array.from(event.target.querySelectorAll(CSS_CLASS_TRAY)).forEach(el => {
-		el.addEventListener(EVENT_SCROLL, setStyleVars);
+const triggerEnterExit = (target, progress, eventEnter, eventExit, className) => {
+	if (progress < 0 || progress > 1) {
+		if (target.classList.contains(className)) {
+			target.classList.remove(className);
+
+			target.dispatchEvent(
+				new CustomEvent(eventExit, {
+					bubbles: true,
+					cancelable: true,
+					composed: false,
+				}),
+			);
+		}
+	} else if (!target.classList.contains(className)) {
+		target.classList.add(className);
+
+		target.dispatchEvent(
+			new CustomEvent(eventEnter, {
+				bubbles: true,
+				cancelable: true,
+				composed: false,
+			}),
+		);
+	}
+};
+
+const triggerInnerEnterExit = ({ target, detail: { progress: { inner } } }) => {
+	triggerEnterExit(target, inner, EVENT_INNER_ENTER, EVENT_INNER_EXIT, CSS_CLASS_INSIDE_INNER);
+};
+
+const triggerOuterEnterExit = ({ target, detail: { progress: { outer } } }) => {
+	triggerEnterExit(target, outer, EVENT_OUTER_ENTER, EVENT_OUTER_EXIT, CSS_CLASS_INSIDE_OUTER);
+};
+
+const scroll = ({ target }) => {
+	Array.from(target.querySelectorAll(SEL_TRAY)).forEach(el => {
 		processSection(el);
+	});
+};
+
+const addSectionListeners = parent => {
+	Array.from(parent.querySelectorAll(SEL_TRAY)).forEach(el => {
+		el.addEventListener(EVENT_SCROLL, setStyleVars);
+		el.addEventListener(EVENT_SCROLL, triggerOuterEnterExit);
+		el.addEventListener(EVENT_SCROLL, triggerInnerEnterExit);
 	});
 };
 
 const scrollerful = () => {
 	addStyle();
 
-	Array.from(document.querySelectorAll(CSS_CLASS_SNAP)).forEach(target => {
+	Array.from(document.querySelectorAll(SEL_SNAP)).forEach(target => {
 		target.addEventListener('resize', scroll);
 		target.addEventListener('scroll', scroll);
+		addSectionListeners(target);
 		scroll({ target });
 	});
 
 	window.addEventListener('resize', scroll);
 	window.addEventListener('scroll', scroll);
+	addSectionListeners(document);
 	scroll({ target: document });
 };(function init() {
 	if (document.readyState === 'interactive') {
