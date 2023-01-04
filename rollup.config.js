4ccb31e7337725814dedb6f823c462e6b52ee1d0
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-04 20:57:59 +0900

    Add bundled JS file

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,29 @@
 import sass from 'rollup-plugin-sass'
+import uglify from '@lopatnov/rollup-plugin-uglify'
 
-export default {
-	input: 'source/js/index.js',
-	output: {
-		compact: true,
-		file: 'source/scrollerful/script.js',
-		format: 'iife',
+const sassPlugin = sass({
+	options: {
+		outputStyle: 'compressed',
 	},
-	plugins: [sass({
-		options: {
-			outputStyle: 'compressed',
+})
+
+export default [
+	{
+		input: 'lib/init.js',
+		output: {
+			compact: true,
+			file: 'dist/scrollerful.js',
+			format: 'iife',
 		},
-	})],
-}
+		plugins: [sassPlugin, uglify()],
+	},
+	{
+		input: 'source/js/index.js',
+		output: {
+			compact: true,
+			file: 'source/scrollerful/script.js',
+			format: 'iife',
+		},
+		plugins: [sassPlugin, uglify()],
+	},
+]
