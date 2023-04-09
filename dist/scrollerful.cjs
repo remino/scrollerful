e0a4a5c13812b2e9e4dfda1512f432ec3d1c5812
Rémino Rem <29999+remino@users.noreply.github.com>
2023-04-09 13:02:10 +0900

    Update dist files, fix version number

--- dist/scrollerful.cjs
+++ dist/scrollerful.cjs
@@ -1,4 +1,4 @@
-/*! scrollerful v0.5.1 | (c) 2022-2023 Rémino Rem <https://remino.net/> | ISC Licence */
+/*! scrollerful v0.6.1 | (c) 2022-2023 Rémino Rem <https://remino.net/> | ISC Licence */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
