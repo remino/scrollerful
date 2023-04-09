dd55cf3fb7583c2be3730ac311788f1cbfb019f6
Rémino Rem <29999+remino@users.noreply.github.com>
2023-04-09 12:52:34 +0900

    Add test suite for new calculations

--- spec/src/calc.spec.js
+++ spec/src/calc.spec.js
@@ -3,17 +3,34 @@ import { calcInnerProgress, calcOuterProgress } from '../../src/calc.js'
 describe('src/calc.js', () => {
 	describe('calcInnerProgress()', () => {
 		const cases = [
-			// containerStart, containerSize, viewSize, expected
-			[0, 200, 100, 0],
-			[-100, 200, 100, 1],
-			[-200, 200, 100, 2],
-			[-300, 200, 100, 3],
+			// testNumber, containerStart, containerSize, viewSize, expected
+
+			// Container taller than viewport
+			['1.01', 100, 200, 100, -1],
+			['1.02', 0, 200, 100, 0],
+			['1.03', -50, 200, 100, 0.5],
+			['1.04', -100, 200, 100, 1],
+			['1.05', -200, 200, 100, 2],
+
+			// Container shorter than viewport
+			['1.06', 100, 50, 100, -1],
+			['1.07', 50, 50, 100, 0],
+			['1.08', 25, 50, 100, 0.5],
+			['1.09', 0, 50, 100, 1],
+			['1.10', -50, 50, 100, 2],
+
+			// Container of same height than viewport
+			['1.11', 100, 100, 100, -1],
+			['1.12', 50, 100, 100, -0.5],
+			['1.13', 0, 100, 100, 0.5],
+			['1.14', -50, 100, 100, 1.5],
+			['1.15', -100, 100, 100, 2],
 		]
 
 		cases.forEach(([
-			containerStart, containerSize, viewSize, expected,
+			testNumber, containerStart, containerSize, viewSize, expected,
 		]) => {
-			it(`returns inner progress of ${expected} for containerStart ${containerStart}, containerSize ${containerSize}, and viewSize ${viewSize}`, () => {
+			it(`(${testNumber}) returns inner progress of ${expected} for containerStart ${containerStart}, containerSize ${containerSize}, and viewSize ${viewSize}`, () => {
 				expect(calcInnerProgress(containerStart, containerSize, viewSize)).toBe(expected)
 			})
 		})
@@ -21,17 +38,34 @@ describe('src/calc.js', () => {
 
 	describe('calcOuterProgress()', () => {
 		const cases = [
-			// containerStart, containerSize, viewSize, expected
-			[100, 200, 100, 0],
-			[0, 200, 100, 1 / 3],
-			[-100, 200, 100, 2 / 3],
-			[-200, 200, 100, 1],
+			// testNumber, containerStart, containerSize, viewSize, expected
+
+			// Container taller than viewport
+			['2.01', 100, 200, 100, 0],
+			['2.02', 0, 200, 100, 1 / 3],
+			['2.03', -50, 200, 100, 0.5],
+			['2.04', -100, 200, 100, 2 / 3],
+			['2.05', -200, 200, 100, 1],
+
+			// Container shorter than viewport
+			['2.06', 100, 50, 100, 0],
+			['2.07', 50, 50, 100, 1 / 3],
+			['2.08', 25, 50, 100, 0.5],
+			['2.09', 0, 50, 100, 2 / 3],
+			['2.10', -50, 50, 100, 1],
+
+			// Container of same height than viewport
+			['2.11', 100, 100, 100, 0],
+			['2.12', 50, 100, 100, 0.25],
+			['2.13', 0, 100, 100, 0.5],
+			['2.14', -50, 100, 100, 0.75],
+			['2.15', -100, 100, 100, 1],
 		]
 
 		cases.forEach(([
-			containerStart, containerSize, viewSize, expected,
+			testNumber, containerStart, containerSize, viewSize, expected,
 		]) => {
-			it(`returns inner progress of ${expected} for containerStart ${containerStart}, containerSize ${containerSize}, and viewSize ${viewSize}`, () => {
+			it(`(${testNumber}) returns outer progress of ${expected} for containerStart ${containerStart}, containerSize ${containerSize}, and viewSize ${viewSize}`, () => {
 				expect(calcOuterProgress(containerStart, containerSize, viewSize)).toBe(expected)
 			})
 		})
